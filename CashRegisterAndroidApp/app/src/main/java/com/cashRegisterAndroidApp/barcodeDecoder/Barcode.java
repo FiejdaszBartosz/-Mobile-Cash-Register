6efe16ba151bd6a11beec9f493a3b1f88f0c3aea
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-03-21 16:31:34 +0100

    Add decoder

---
+++ CashRegisterAndroidApp/app/src/main/java/com/cashRegisterAndroidApp/barcodeDecoder/Barcode.java
@@ -0,0 +1,6 @@
+package com.cashRegisterAndroidApp.barcodeDecoder;
+
+public abstract class Barcode {
+    public static final int BARCODE_WIDTH = 400;
+    public static final int BARCODE_HEIGHT = 200;
+}
---
+++ CashRegisterAndroidApp/app/src/main/java/com/cashRegisterAndroidApp/barcodeDecoder/BarcodeReader.java
@@ -0,0 +1,39 @@
+package com.cashRegisterAndroidApp.barcodeDecoder;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.google.zxing.aztec.detector.Detector;
+
+public class BarcodeReader implements Detector.Processor<Barcode> {
+
+    private Context mContext;
+    private TextView mResultTextView;
+
+    public BarcodeReader(Context context, TextView resultTextView) {
+        mContext = context;
+        mResultTextView = resultTextView;
+    }
+
+    @Override
+    public void release() {
+        // Do nothing
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<Barcode> detections) {
+        SparseArray<Barcode> barcodes = detections.getDetectedItems();
+        if (barcodes.size() != 0) {
+            Barcode barcode = barcodes.valueAt(0);
+            if (barcode != null) {
+                String scannedCode = barcode.rawValue;
+                if (scannedCode != null && !scannedCode.isEmpty()) {
+                    mResultTextView.post(() -> {
+                        mResultTextView.setText(scannedCode);
+                        Toast.makeText(mContext, "Code scanned: " + scannedCode, Toast.LENGTH_SHORT).show();
+                    });
+                }
+            }
+        }
+    }
+}
