140fe7769baeb5e84f7c7ac227910d48ac1c4a1e
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-04-04 13:58:42 +0200

    CameraX button is working

--- CashRegisterAndroidApp/app/src/main/java/com/cashRegisterAndroidApp/cameraModule/CameraX.java
+++ CashRegisterAndroidApp/app/src/main/java/com/cashRegisterAndroidApp/cameraModule/CameraX.java
@@ -1,14 +1,10 @@
 package com.cashRegisterAndroidApp.cameraModule;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Size;
-import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,12 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
-import androidx.camera.core.UseCase;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
@@ -35,6 +30,7 @@ public class CameraX extends AppCompatActivity {
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageCapture imageCapture;
+    private Button button;
     //private TextView textView;
 
     @Override
@@ -42,26 +38,63 @@ public class CameraX extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_x_layout);
         previewView = findViewById(R.id.PreviewView);
+        button = findViewById(R.id.image_capture_button);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         //textView = findViewById(R.id.orientation);
+        ProcessCameraProvider cameraProvider = null;
+        try {
+            cameraProvider = cameraProviderFuture.get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         ImageCapture.Builder builder = new ImageCapture.Builder();
         builder.setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY);
-        builder.setIoExecutor(ContextCompat.getMainExecutor(this));
+        builder.setIoExecutor(ContextCompat.getMainExecutor(CameraX.this));
         imageCapture = builder.build();
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,
+                imageAnalysis, preview, imageCapture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imageCapture.takePicture(ContextCompat.getMainExecutor(CameraX.this), new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy image) {
+                        Log.i("123", "Button dziala");
+                        image.close();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        exception.printStackTrace();
+                    }
+                });
+            }
+        });
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
-                try {
-                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindImageAnalysis(cameraProvider);
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(CameraX.this), new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(@NonNull ImageProxy image) {
+                        image.close();
+                    }
+                });
+
             }
         }, ContextCompat.getMainExecutor(this));
     }
 
-    private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
+    private void bind(@NonNull ProcessCameraProvider cameraProvider) {
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
@@ -71,19 +104,21 @@ public class CameraX extends AppCompatActivity {
                 image.close();
             }
         });
-        OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
-            @Override
-            public void onOrientationChanged(int orientation) {
-                //textView.setText(Integer.toString(orientation));
-            }
-        };
-        orientationEventListener.enable();
         Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,
-                imageAnalysis, preview, imageCapture);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,
+                imageAnalysis, preview);
     }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
+
 
 }
