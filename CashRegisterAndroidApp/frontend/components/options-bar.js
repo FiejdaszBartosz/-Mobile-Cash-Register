9c8755c0bc144a77e645668e824f84cf614daa81
Ruffaaw <rafalciupek07@gmail.com>
2023-05-31 19:52:28 +0200

    Updated options bar

--- CashRegisterAndroidApp/frontend/components/options-bar.js
+++ CashRegisterAndroidApp/frontend/components/options-bar.js
@@ -15,24 +15,17 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 const OptionsBar = () => {
   return (
     <View style={style.container}>
-      <TouchableOpacity>
-        <FontAwesome name="shopping-cart" style={style.icons} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          width: 116,
-          height: 116,
-          backgroundColor: "#E63946",
-          borderRadius: 80,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <FontAwesome name="camera" style={style.iconsCamera} />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <FontAwesome name="user" style={style.icons} />
-      </TouchableOpacity>
+      <View style={style.mainContainer}>
+        <TouchableOpacity style={style.touchableIcons}>
+          <FontAwesome name="shopping-cart" style={style.icons} />
+        </TouchableOpacity>
+        <TouchableOpacity style={style.touchableCamera}>
+          <FontAwesome name="camera" style={style.iconsCamera} />
+        </TouchableOpacity>
+        <TouchableOpacity style={style.touchableIcons}>
+          <FontAwesome name="user" style={style.icons} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -41,18 +34,30 @@ export default OptionsBar;
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
     width: "100%",
-    height: 73,
+    height: 70,
+    backgroundColor: "#457B9D",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0,
+  },
+  mainContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-    backgroundColor: "#457B9D",
   },
   icons: {
     fontSize: 32,
   },
   iconsCamera: {
     fontSize: 35,
+  },
+  touchableCamera: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#E63946",
+    borderRadius: 80,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
