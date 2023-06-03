9c290255beb3894c13fc0f869302e1e020529289
FiejdaszBartosz <bartosz.fiejdasz1@gmail.com>
2023-06-03 15:43:57 +0200

    Added database

---
+++ cashregi/cashregi/src/main/java/com/cashregi/cashregi/entity/User.java
@@ -0,0 +1,109 @@
+package com.cashregi.cashregi.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "User")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "user_email_unique", columnNames = "email")
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+            name = "id"
+    )
+    private UUID id;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "password",
+            nullable = false
+    )
+    private String password;
+
+    @Column(
+            name = "name",
+            nullable = false
+    )
+    private String name;
+
+    @Column(
+            name = "surname",
+            nullable = false
+    )
+    private String surname;
+
+    @JsonIgnore
+    @OneToMany(
+            mappedBy = "user",
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private List<Receipt> receipts = new ArrayList<>();
+
+    public User(){}
+    public User(String email, String password, String name, String surname) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
---
+++ cashregi/cashregi/src/main/java/com/cashregi/cashregi/service/UserService.java
@@ -0,0 +1,45 @@
+package com.cashregi.cashregi.service;
+
+import com.cashregi.cashregi.Controller.LoginRequest;
+import com.cashregi.cashregi.Controller.RegisterRequest;
+import com.cashregi.cashregi.entity.Receipt;
+import com.cashregi.cashregi.entity.User;
+import com.cashregi.cashregi.repository.ReceiptRepository;
+import com.cashregi.cashregi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private ReceiptRepository receiptRepository;
+    @Autowired
+    public UserService(UserRepository userRepository, ReceiptRepository receiptRepository) {
+        this.userRepository = userRepository;
+        this.receiptRepository = receiptRepository;
+    }
+
+    public User login(LoginRequest request){
+        return userRepository.getUserByEmailAndPassword(request.getEmail(), request.getPassword()).orElse(null);
+    }
+
+    public void register(RegisterRequest request){
+        User user = new User(request.getEmail(), request.getPassword(), request.getFirstName(), request.getLastName());
+        userRepository.save(user);
+    }
+
+    public Receipt addNewReceipt(UUID user_id){
+        User user = userRepository.getUserByEmail(user_id).orElse(null);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        Receipt receipt = new Receipt(user, currentDateTime, null, 0.0);
+        receiptRepository.save(receipt);
+
+        return receipt;
+    }
+}
